b9924e48aa3ac711c3627c60ee5e577f77042579
shockliang <shockwater@gmail.com>
2022-04-03 00:23:17 +0800

    Adding sign up form styles.

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -5,6 +5,8 @@ import {
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 
+import './sign-up-form.styles.scss';
+
 interface DefaultFormFields {
   displayName: string,
   email: string,
@@ -56,8 +58,9 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className={"sign-up-container"}>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label={"Display Name"}
