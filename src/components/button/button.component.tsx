67d4ff444a0f467fbc83de6a3d51831f97bed7c6
shockliang <shockwater@gmail.com>
2022-04-06 22:21:38 +0800

    Using enum instead of record.

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,19 +1,28 @@
 import {ButtonHTMLAttributes, ReactChildren} from "react";
 import './button.styles.scss';
 
-const BUTTON_TYPE_CLASSES:Record<string, string> = {};
-BUTTON_TYPE_CLASSES.google = 'google-sign-in';
-BUTTON_TYPE_CLASSES.inverted = 'inverted';
+export enum ButtonStyle {Google, Inverted}
+
+function getButtonTypeName(buttonType: ButtonStyle | undefined) {
+  switch (buttonType) {
+    case ButtonStyle.Google:
+      return 'google-sign-in'
+    case ButtonStyle.Inverted:
+      return 'inverted'
+    default:
+      return ''
+  }
+}
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactChildren | ReactChildren[] | string | string[],
-  buttonType?: string
+  buttonType?: ButtonStyle
 }
 
 const Button = ({children, buttonType, ...otherProps}: ButtonProps) => {
   return (
     <button
-      className={`button-container ${(buttonType === undefined ? '' : BUTTON_TYPE_CLASSES[buttonType])}`}
+      className={`button-container ${getButtonTypeName(buttonType)}`}
       {...otherProps}
     >
       {children}
--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -73,7 +73,7 @@ const SignInForm = () => {
         />
         <div className={"buttons-container"}>
           <Button type={"submit"}>Sign In</Button>
-          <Button type={"button"} onClick={signInWithGoogleUser} buttonType={'google'}>
+          <Button type={"button"} onClick={signInWithGoogleUser} buttonType={ButtonStyle.Google}>
             Google Sign In
           </Button>
         </div>
