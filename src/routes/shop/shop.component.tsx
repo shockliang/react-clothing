67d94152d0f24aef9bcd82647acd38ad74554db1
shockliang <shockwater@gmail.com>
2022-04-23 00:34:35 +0800

    Using category map.

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,17 +1,30 @@
-import {useContext} from "react";
-import {ProductsContext} from "../../contexts/products.context";
+import {Fragment, useContext} from "react";
+import {CategoriesContext} from "../../contexts/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 import './shop.styles.scss';
 
 const Shop = () => {
-  const {products} = useContext(ProductsContext);
+  const {categoryMap} = useContext(CategoriesContext);
+  let titles: string[] = [];
+  categoryMap.forEach((value, key) => titles.push(key));
   return (
-    <div className={"products-container"}>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product}/>
-      ))}
-    </div>
-  )
-}
+    <Fragment>
+      {
+        titles.map((title) => (
+          <Fragment key={title}>
+            <h2>{title}</h2>
+            <div className={"products-container"}>
+              {
+                categoryMap.get(title)!.map((product) => (
+                  <ProductCard key={product.id} product={product}/>
+                ))
+              }
+            </div>
+          </Fragment>
+        ))
+      }
+    </Fragment>
+  );
+};
 
 export default Shop;
