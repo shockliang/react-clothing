319bc6f090070621dbfec0c561e5f46f1574d8fc
shockliang <shockwater@gmail.com>
2022-05-14 19:30:37 +0800

    Categories reselect.

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,11 +1,21 @@
 import {RootState} from "../root-reducer";
 import {Product} from "../../models/product";
+import {createSelector} from "reselect";
 
-export const selectCategoriesMap = (state: RootState) => state.categories.categories
-  .reduce((acc, category) => {
-    const {title, items} = category;
+const selectCategoryReducer = (state: RootState) => state.categories;
 
-    acc.set(title.toLowerCase(), items);
-    return acc;
-  }, new Map<string, Product[]>());
-;
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories
+    .reduce((acc, category) => {
+      const {title, items} = category;
+
+      acc.set(title.toLowerCase(), items);
+      return acc;
+    }, new Map<string, Product[]>())
+);
