4bd1eb4361fd5a169e0bb1d601a121f15606d120
shockliang <shockwater@gmail.com>
2022-05-16 23:34:53 +0800

    Redux-persist.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,20 @@
 import {compose, createStore, applyMiddleware} from "redux";
 import logger from "redux-logger";
 import {rootReducer} from "./root-reducer";
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blocklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
+export const persistor = persistStore(store);
