bcdcc9271c0970cc08755385728a172c0e8cbb0c
shockliang <shockwater@gmail.com>
2022-06-18 16:09:57 +0800

    Convert product card style scss to styled.

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,9 +1,10 @@
-import './product-card.styles.scss';
+import './product-card.styles';
 import Button, {ButtonStyle} from "../button/button.component";
 import {Product} from "../../models/product";
 import {useDispatch, useSelector} from "react-redux";
 import {addItemToCart} from "../../store/cart/cart.action";
 import {selectCartItems} from "../../store/cart/cart.selector";
+import {Footer, ProductCardContainer, Name, Price} from "./product-card.styles";
 
 interface ProductCardProps {
   product: Product
@@ -17,14 +18,14 @@ const ProductCard = ({product}: ProductCardProps) => {
   const addProductToCart = () =>dispatch(addItemToCart(cartItems, product));
 
   return (
-    <div className={"product-card-container"}>
+    <ProductCardContainer>
       <img src={imageUrl} alt={`${name}`}/>
-      <div className={"footer"}>
-        <span className={"name"}>{name}</span>
-        <span className={"price"}>{price}</span>
-      </div>
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
       <Button buttonType={ButtonStyle.Inverted} onClick={addProductToCart}>Add to cart</Button>
-    </div>
+    </ProductCardContainer>
   )
 }
 
