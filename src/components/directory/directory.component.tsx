438d43f9cff19de5e2848e2a4df28b56cc499dae
shockliang <shockwater@gmail.com>
2022-06-19 19:51:05 +0800

    Convert directory style scss to styled.

--- src/components/directory/directory.component.tsx
+++ src/components/directory/directory.component.tsx
@@ -1,6 +1,7 @@
 import DirectoryItem from "../directory-item/directory-item.component";
 import {Category} from "../../models/category";
-import "./directory.styles.scss";
+import "./directory.styles";
+import {DirectoryContainer} from "./directory.styles";
 
 const categories: Category[] = [
   {
@@ -37,11 +38,11 @@ const categories: Category[] = [
 
 const Directory = () => {
   return (
-    <div className={"directory-container"}>
+    <DirectoryContainer>
       {categories.map((category) => (
         <DirectoryItem key={category.id} category={category}/>
       ))}
-    </div>
+    </DirectoryContainer>
   )
 }
 
