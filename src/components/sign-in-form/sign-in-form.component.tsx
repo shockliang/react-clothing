a57cf6da864155f09e437933ff15cfca8d0af9f4
shockliang <shockwater@gmail.com>
2022-07-19 23:08:46 +0800

    NextUI input in sign in form.

--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -1,11 +1,13 @@
 import {ChangeEvent, FormEvent, useState} from "react";
-import FormInput from "../form-input/form-input.component";
+import React from "react";
 import Button, {ButtonStyle} from "../button/button.component";
-
 import './sign-in-form.styles';
 import {useDispatch} from "react-redux";
 import {emailSignInStart, googleSignInStart} from "../../store/user/user.action";
 import {ButtonsContainer, SignUpContainer} from "./sign-in-form.styles";
+import { Input, useInput, Grid } from "@nextui-org/react";
+import {FormElement} from "@nextui-org/react/types/input/input-props";
+
 
 interface DefaultFormFields {
   email: string,
@@ -17,12 +19,20 @@ const defaultFormFields: DefaultFormFields = {
   password: '',
 }
 
+type SimpleColors =
+  | 'default'
+  | 'primary'
+  | 'secondary'
+  | 'success'
+  | 'warning'
+  | 'error';
+
 const SignInForm = () => {
   const dispatch = useDispatch();
   const [formFields, setFormFields] = useState<DefaultFormFields>(defaultFormFields);
   const {email, password} = formFields;
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<FormElement>) => {
     const {name, value} = event.target;
     setFormFields({...formFields, [name]: value});
   }
@@ -52,23 +62,31 @@ const SignInForm = () => {
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <FormInput
-          label={"Email"}
-          type={"email"}
-          required
-          onChange={handleChange}
-          name={"email"}
-          value={email}
-        />
-
-        <FormInput
-          label={"Password"}
-          type={"password"}
-          required
-          onChange={handleChange}
-          name={"password"}
-          value={password}
-        />
+        <Grid.Container gap={4}>
+          <Grid>
+            <Input
+              clearable
+              helperText="Please enter your email"
+              label="Email"
+              type={"email"}
+              placeholder="Enter your email"
+              onChange={handleChange}
+              required
+              name={"email"}
+            />
+          </Grid>
+          <Grid>
+            <Input.Password
+              clearable
+              color="default"
+              type="password"
+              label="Password"
+              placeholder="Enter your password"
+              onChange={handleChange}
+              required
+            />
+          </Grid>
+        </Grid.Container>
         <ButtonsContainer>
           <Button type={"submit"}>Sign In</Button>
           <Button type={"button"} onClick={signInWithGoogleUser} buttonType={ButtonStyle.Google}>
